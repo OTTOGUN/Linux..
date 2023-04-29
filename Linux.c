58f60098be5f04b11f438e761192aed6b004787e
OTTOGUN <127713240+OTTOGUN@users.noreply.github.com>
2023-04-29 21:46:35 +0800

    Add files via upload

---
+++ Linux.c
@@ -0,0 +1,11 @@
+#!bin/bash
+# Program:
+#           Using for ... loop to print 3 animals
+# History
+# 2023.4.29         OTTO        First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+for animal in dog cat elephant
+do
+    echo "There are ${amimal}s..."
+done
